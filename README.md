07c49682770732d8c82f44d0123fde019b807c68
lsh0520 <lsh0520@mail.ustc.edu.cn>
2024-01-23 16:44:06 +0800

    add README

--- README.md
+++ README.md
@@ -1,23 +1,145 @@
 # 3D-MoLM: Towards 3D Molecule-Text Interpretation in Language Models
 
-## We will release the checkpoints and curated datasets once accepted
+Codes of our ICLR'24 paper. [[Paper Link]()], [[Website](https://lsh0520.github.io/3D-MoLM/)], [[Demo]()]
+
+Authors: Sihang Li*, Zhiyuan Liu*, Yanchen Luo, Xiang Wang†, Xiangnan He†, Kenji Kawaguchi, Tat-Seng Chua, Qi Tian
+
+\* Equal Contribution    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  † Corresponding
+
+## Framework
+
+<p align="center">
+  <img src="./figures/framework.png" alt="fig1" width="70%"/>
+</p>
+
+* 3D-MoLM is a <b>versatile</b> molecular LM that can be applied for molecule-text retrieval, molecule captioning, and molecule question-answering tasks.
+                
+
+* 3D-MoLM employs a 3D molecule-text <b>projector</b> to bridge the modality gap between a 3D molecular encoder and an LM, enabling the LM to <b>perceive 3D molecular structures</b>.
+
+## Training pipeline
+
+<p align="center">
+  <img src="./figures/train_pipeline.png" alt="fig2" width="70%"/>
+</p>
+
+To tackle the two challenges of <b>3D molecule-text alignment</b> and <b>3D molecule-centric instruction tuning</b>, we delineate a three-stage training pipeline for 3D-MoLM, including 1) 3D molecule-text representation learning, 2) 3D molecule-text alignment via text generation, and 3) instruction-based fine-tuning.
+                
+### 3D Molecule-Text Alignment
+3D Molecule-Text Alignment maps 3D molecular representations into the input textual space where the LM can understand.
+                
+* <b>Data Preparation – PubChem.</b> We collect molecular SMILES-text pairs from PubChem, and further employ GPT-3.5 to enrich the less annotated molecular descriptions. Molecular 3D conformations are obtained by running the MMFF algorithm in RDKit. As a result, 316K 3D molecule-text pairs are obtained for the alignment pretraining and downstream tasks of molecule-text retrieval and molecule captioning.
+
+* <b><span>Stage 1</span>: 3D Molecule-Text Representation Learning</b> cultivates Q-Former’s ability to extract molecular features that resonate profoundly with the corresponding tex</li>
+
+* <b><span>Stage 2</span>: 3D Molecule-Text Alignment via Generative Learning</b> connect the Q-Former with the LM to leverage the LM’s language generation capability, where the molecular representations extracted by the Q-Former serve as 1D soft prompts comprehensible to the LM.
+                       
+### 3D Molecule-centric Instruction Tuning      
+3D Molecule-centric Instruction Tuning fine-tunes the model to follow human instructions on 3D molecule relevant tasks.
+
+* <b>Data Preparation – 3D-MoIT.</b> We construct 3D-MoIT by sourcing data from the PubChem and PubChemQC databases, including both computed molecular properties and descriptive texts that characterize molecule properties in numerical values and free texts, respectively.
+
+* <b><span>Stage 3</span>: Instruction-based Fine-tuning</b> enhances the ability to follow various instructions and improves the understanding of 3D molecular structures, especially in recognizing 3D-dependent properties          
+
+## Demonstration
+
+### Molecule Captioning
+<p align="left">
+  <img src="./figures/mol-cap.png" alt="fig3" width="50%"/>
+</p>
+
+### Open-text QA
+<p align="left">
+  <img src="./figures/open-text-qa.png" alt="fig4" width="50%"/>
+</p>
+
+### Computed Property QA
+<p align="left">
+  <img src="./figures/computed-property-qa.png" alt="fig5" width="50%"/>
+</p>
+
 
 ## Requirements
 
-python==3.8
+See `requirements.txt`. Run the following command to create a new anaconda environment `3D-MoLM`: 
 
-* Install PyTorch with cuda-11.7 using conda by following the instructions in [link](https://pytorch.org/get-started/locally/)
-* Install flash-attention by running `pip install flash-attn --no-build-isolation`. You might need to install the following dependencies for building the flash-attention module:
-    * `pip install packaging ninja`
-    * `conda install -c "nvidia/label/cuda-11.7.1" cuda-nvcc`
-    * `conda install -c "nvidia/label/cuda-11.7.1" cuda-libraries-dev`
-* Install the lastest version of opendela by runing `pip install git+https://github.com/thunlp/OpenDelta.git`
-* Install lavis: `pip install rouge_score nltk salesforce-lavis`
-* Install others: `pip install -U transformers pytorch-lightning`
-* Install the lastest version of deepspeed: `pip install git+https://github.com/microsoft/DeepSpeed.git`
-* Download nltk corpus:
-```
-import nltk
-nltk.download('wordnet')
+```bash
+pip install -r requirements.txt
 ```
 
+## Dataset
+
+* **3D-MoIT**. Download the dataset from [Huggingface-3D-MoIT](https://huggingface.co/datasets/Sihangli/3D-MoIT), and put it under the `./data/` directory.
+
+## Checkpoints
+* **3D-MoLM**. Download the LoRA checkpoints from [Huggingface-3D-MoLM](https://huggingface.co/Sihangli/3D-MoLM), and put it under the `./all_checkpoints/` directory.
+
+* **Uni-Mol**. Download the checkpoint from [Huggingface-Uni-Mol](https://huggingface.co/Sihangli/3D-MoLM), and put it under the `./all_checkpoints/` directory.
+
+* **SciBERT**. Download the checkpoint from [Huggingface-scibert](https://huggingface.co/allenai/scibert_scivocab_uncased), and put it under the `./all_checkpoints/` directory.
+
+* **Llama2-7B**. Download the checkpoint from [Huggingface-Llama](https://huggingface.co/meta-llama/Llama-2-7b-hf), and put it under the `./all_checkpoints/` directory.
+
+
+## Reproduce the results
+### Prerequisites
+* Install required conda environment as described in <b>Requirements</b> section
+* Download the dataset and required checkpoints as described in <b>Dataset</b> and <b>Checkpoints</b> section.
+
+### Directly Read the Outputs
+We provide the outputs of 3D-MoLM on the test set of 3D-MoIT in `all_checkpoints/generalist/lightning_logs/version_0/predictions.txt`. Run the following script to read it.
+```bash
+python read_generalist_results.py 
+    --file_path 'all_checkpoints/generalist/lightning_logs/version_0/predictions.txt' 
+    --tokenizer_path 'all_checkpoints/llama-2-7b-hf'
+```
+
+### Evaluation on Our Pretrained Checkpoint
+
+We share the checkpoint for reproducing results.
+
+
+```bash
+bash ./scripts/stage3_test.sh
+```
+
+### Training the Model from Scratch
+
+**Stage 1: 3D Molecule-Text Representation Learning** 
+
+Run the following script for stage 1 pretraining:
+
+```bash
+bash ./scripts/stage1_pretrain.sh
+```
+
+**Stage 2: 3D Molecule-Text Alignment via Generative Learning** 
+
+Run the following script for stage 2 pretraining:
+
+```bash
+bash ./scripts/stage2_pretrain.sh
+```
+
+**Stage 3: Instruction-based Fine-tuning** 
+
+Run the following script for instruction tuning:
+
+```bash
+bash ./scripts/stage3_train.sh
+```
+
+
+## Citation
+
+If you use our codes or checkpoints, please cite our paper:
+
+```bib
+@inproceedings{li2024iclr,
+    title={3D-MoLM: Towards 3D Molecule-Text Interpretation in Language Models},
+    author={Li, Sihang and Liu, Zhiyuan and Luo, Yanchen and Wang, Xiang and He, Xiangnan and Kawaguchi, Kenji  and Chua, Tat-Seng and Tian, Qi},
+    booktitle={ICLR},
+    year={2024},
+    url={https://openreview.net/forum?id=xI4yNlkaqh}
+}
+```
--- read_generalist_results.py
+++ read_generalist_results.py
@@ -5,121 +5,128 @@ import re
 import numpy as np
 from collections import Counter
 from model.help_funcs import caption_evaluate
+import argparse
 
-root = 'all_checkpoints/generalist/lightning_logs/version_0'
-file_path = os.path.join(root, 'predictions.txt')
+def main(file_path, tokenizer_path):
+    dicts = []
+    with open(file_path, 'r', encoding='utf8') as f:
+        for line in f:
+            dicts.append(json.loads(line.strip()))
 
-dicts = []
-with open(file_path, 'r', encoding='utf8') as f:
-    for line in f:
-        dicts.append(json.loads(line.strip()))
+    _prediction = []
+    _targets = []
+    _task_type = []
+    for _dict in dicts:
+        _prediction.extend(_dict['prediction'])
+        _targets.extend(_dict['target'])
+        _task_type.extend(_dict['task_type'])
 
-_prediction = []
-_targets = []
-_task_type = []
-for _dict in dicts:
-    _prediction.extend(_dict['prediction'])
-    _targets.extend(_dict['target'])
-    _task_type.extend(_dict['task_type'])
+    task_type_counter = Counter(_task_type)
+    prediction = {}
+    targets = {}
 
-task_type_counter = Counter(_task_type)
-prediction = {}
-targets = {}
+    prediction['HOMO'] = []
+    prediction['LUMO'] = []
+    prediction['HOMO-LUMO Gap'] = []
+    prediction['SCF Energy'] = []
 
-prediction['HOMO'] = []
-prediction['LUMO'] = []
-prediction['HOMO-LUMO Gap'] = []
-prediction['SCF Energy'] = []
+    prediction['Molecular Weight'] = []
+    prediction['LogP'] = []
+    prediction['Topological Polar Surface Area'] = []
+    prediction['Complexity'] = []
 
-prediction['Molecular Weight'] = []
-prediction['LogP'] = []
-prediction['Topological Polar Surface Area'] = []
-prediction['Complexity'] = []
+    prediction['Description'] = []
+    prediction['Caption'] = []
 
-prediction['Description'] = []
-prediction['Caption'] = []
+    targets['HOMO'] = []
+    targets['LUMO'] = []
+    targets['HOMO-LUMO Gap'] = []
+    targets['SCF Energy'] = []
 
-targets['HOMO'] = []
-targets['LUMO'] = []
-targets['HOMO-LUMO Gap'] = []
-targets['SCF Energy'] = []
+    targets['Molecular Weight'] = []
+    targets['LogP'] = []
+    targets['Topological Polar Surface Area'] = []
+    targets['Complexity'] = []
 
-targets['Molecular Weight'] = []
-targets['LogP'] = []
-targets['Topological Polar Surface Area'] = []
-targets['Complexity'] = []
+    targets['Description'] = []
+    targets['Caption'] = []
 
-targets['Description'] = []
-targets['Caption'] = []
+    pattern = r'-?\d+\.\d+'
+    # pattern = r'(-?\d+\.\d+|-?\d+)'
 
-pattern = r'-?\d+\.\d+'
-# pattern = r'(-?\d+\.\d+|-?\d+)'
+    e_prediction = []
+    e_target = []
 
-e_prediction = []
-e_target = []
+    for i, t in enumerate(_task_type):
+        if t in ['Description', 'Caption']:
+            prediction[t].append(_prediction[i])
+            targets[t].append(_targets[i])
+        else:
+            pre_matches = re.findall(pattern, _prediction[i])
+            tar_matches = re.findall(pattern, _targets[i])
+            if t in ['HOMO', 'LUMO', 'HOMO-LUMO Gap']:
+                if len(pre_matches) > 0 and -20 < float(pre_matches[0]) < 20:
+                    prediction[t].append(float(pre_matches[0]))
+                    targets[t].append(float(tar_matches[0]))
+                else:
+                    e_prediction.append(_prediction[i])
+                    e_target.append(_targets[i])
+            elif t in ['SCF Energy']:
+                if len(pre_matches) > 0 and -5 < float(pre_matches[0]) < 0:
+                    prediction[t].append(float(pre_matches[0]))
+                    targets[t].append(float(tar_matches[0]))
+                else:
+                    e_prediction.append(_prediction[i])
+                    e_target.append(_targets[i])
+            elif t in ['LogP']:
+                if len(pre_matches) > 0 and -30 < float(pre_matches[0]) < 50:
+                    prediction[t].append(float(pre_matches[0]))
+                    targets[t].append(float(tar_matches[0]))
+                else:
+                    e_prediction.append(_prediction[i])
+                    e_target.append(_targets[i])
+            elif t in ['Topological Polar Surface Area']:
+                if len(pre_matches) > 0 and 0 <= float(pre_matches[0]) < 2000:
+                    prediction[t].append(float(pre_matches[0]))
+                    targets[t].append(float(tar_matches[0]))
+                else:
+                    e_prediction.append(_prediction[i])
+                    e_target.append(_targets[i])
+            elif t in ['Complexity']:
+                if len(pre_matches) > 0 and 0 <= float(pre_matches[0]) < 10000:
+                    prediction[t].append(float(pre_matches[0]))
+                    targets[t].append(float(tar_matches[0]))
+                else:
+                    e_prediction.append(_prediction[i])
+                    e_target.append(_targets[i])
+            elif t in ['Molecular Weight']:
+                if len(pre_matches) > 0 and 0 < float(pre_matches[0]) < 4000:
+                    prediction[t].append(float(pre_matches[0]))
+                    targets[t].append(float(tar_matches[0]))
+                else:
+                    e_prediction.append(_prediction[i])
+                    e_target.append(_targets[i])
 
-for i, t in enumerate(_task_type):
-    if t in ['Description', 'Caption']:
-        prediction[t].append(_prediction[i])
-        targets[t].append(_targets[i])
-    else:
-        pre_matches = re.findall(pattern, _prediction[i])
-        tar_matches = re.findall(pattern, _targets[i])
-        if t in ['HOMO', 'LUMO', 'HOMO-LUMO Gap']:
-            if len(pre_matches) > 0 and -20 < float(pre_matches[0]) < 20:
-                prediction[t].append(float(pre_matches[0]))
-                targets[t].append(float(tar_matches[0]))
-            else:
-                e_prediction.append(_prediction[i])
-                e_target.append(_targets[i])
-        elif t in ['SCF Energy']:
-            if len(pre_matches) > 0 and -5 < float(pre_matches[0]) < 0:
-                prediction[t].append(float(pre_matches[0]))
-                targets[t].append(float(tar_matches[0]))
-            else:
-                e_prediction.append(_prediction[i])
-                e_target.append(_targets[i])
-        elif t in ['LogP']:
-            if len(pre_matches) > 0 and -30 < float(pre_matches[0]) < 50:
-                prediction[t].append(float(pre_matches[0]))
-                targets[t].append(float(tar_matches[0]))
-            else:
-                e_prediction.append(_prediction[i])
-                e_target.append(_targets[i])
-        elif t in ['Topological Polar Surface Area']:
-            if len(pre_matches) > 0 and 0 <= float(pre_matches[0]) < 2000:
-                prediction[t].append(float(pre_matches[0]))
-                targets[t].append(float(tar_matches[0]))
-            else:
-                e_prediction.append(_prediction[i])
-                e_target.append(_targets[i])
-        elif t in ['Complexity']:
-            if len(pre_matches) > 0 and 0 <= float(pre_matches[0]) < 10000:
-                prediction[t].append(float(pre_matches[0]))
-                targets[t].append(float(tar_matches[0]))
-            else:
-                e_prediction.append(_prediction[i])
-                e_target.append(_targets[i])
-        elif t in ['Molecular Weight']:
-            if len(pre_matches) > 0 and 0 < float(pre_matches[0]) < 4000:
-                prediction[t].append(float(pre_matches[0]))
-                targets[t].append(float(tar_matches[0]))
-            else:
-                e_prediction.append(_prediction[i])
-                e_target.append(_targets[i])
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=False, padding_side='right')
 
-tokenizer = AutoTokenizer.from_pretrained('/mnt/vepfs/fs_users/lisihang/3D-MoLM/all_checkpoints/llama-2-7b-hf', use_fast=False, padding_side='right')
+    for key in prediction.keys():
+        if key in ['Description', 'Caption']:
+            print(f'{key}')
+            bleu2, bleu4, rouge_1, rouge_2, rouge_l, meteor_score = caption_evaluate(prediction[key], targets[key], tokenizer, 128)
+            pass
+        else:
+            valid_ratio = len(prediction[key])/task_type_counter[key]
+            error = np.array(prediction[key]) - np.array(targets[key])
+            mae = np.mean(np.abs(error))
+            print(f'{key}    Valid Ratio:{valid_ratio:.4f}, MAE:{mae:.3f}')
 
-for key in prediction.keys():
-    if key in ['Description', 'Caption']:
-        print(f'{key}')
-        bleu2, bleu4, rouge_1, rouge_2, rouge_l, meteor_score = caption_evaluate(prediction[key], targets[key], tokenizer, 128)
-        pass
-    else:
-        valid_ratio = len(prediction[key])/task_type_counter[key]
-        error = np.array(prediction[key]) - np.array(targets[key])
-        mae = np.mean(np.abs(error))
-        print(f'{key}    Valid Ratio:{valid_ratio:.4f}, MAE:{mae:.3f}')
-exit()
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Read the results of generalist model.")
+    parser.add_argument("--file_path", type=str, default="all_checkpoints/generalist/lightning_logs/version_0/predictions.txt", help="The path to the output file.")
+    parser.add_argument("--tokenizer_path", type=str, default="all_checkpoints/llama-2-7b-hf", help="The path to the tokenizer.")
+    args = parser.parse_args()
+    return args
 
-
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args.file_path, args.tokenizer_path)
