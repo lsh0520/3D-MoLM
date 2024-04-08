ad6d4c47f9024217e5bb25a66d59072e7cdcd1ab
lsh0520 <lsh0520@mail.ustc.edu.cn>
2024-04-08 19:48:28 +0800

    add more ckpts

--- scripts/generalist_test.sh
+++
@@ -1,1 +0,0 @@
-python stage3.py --filename generalist --mode eval --stage3_path all_checkpoints/generalist/generalist.ckpt
---
+++ scripts/stage1_test.sh
@@ -0,0 +1,1 @@
+python stage1.py --filename stage1_eval --mode eval --init_checkpoint all_checkpoints/stage1-ft-ckpt/stage1-ft.ckpt
